29fa04fa978e82a0e6f3a5af69dbd4fa1a6b9051
Mihir Pathak <mihirpathak267@gmail.com>
2023-02-22 10:47:31 +0530

    Add the /articles route.
    Build the /articles route for GET requests such that all the articles
    in the database will be fetched and shown on the /articles page.
    This is made so that the API is RESTful in its architechtural style.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const ejs = require("ejs");
 
 const app = express();
 
@@ -10,11 +11,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 mongoose.set("strictQuery", false);
 
-// main().catch(err => console.log(err));
+main().catch(err => console.log(err));
 
-// async function main() {
-//     await mongoose.connect("mongodb://localhost:27017/wikiDB");
-// };
+async function main() {
+    await mongoose.connect("mongodb://localhost:27017/wikiDB");
+};
 
 const articleSchema = new mongoose.Schema({
     title: String,
@@ -23,8 +24,14 @@ const articleSchema = new mongoose.Schema({
 
 const Article = new mongoose.model("Article", articleSchema);
 
-app.get("/", function(req, res){
-    res.send("This working cuh!");
+app.get("/articles", function(req, res){
+    Article.find({}, function(err, foundArticles){
+        if(!err){
+            res.send(foundArticles);
+        } else{
+            res.send(err);
+        }
+    })
 });
 
 app.listen(3000, function(req, res){
